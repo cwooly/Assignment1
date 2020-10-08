659c0fba1ce030b158c3e9bd5e5d7ac3012d730a
Charles <Charles@Charles-PC>
2020-10-07 20:30:17 -0700

    V2

--- Assignment1/src/cse360assignment02/AddingMachine.java
+++ Assignment1/src/cse360assignment02/AddingMachine.java
@@ -2,23 +2,28 @@ package cse360assignment02;
 
 public class AddingMachine {
   private int total;
+  String history;
   
   public AddingMachine () {
     total = 0;  // not needed - included for clarity
   }
   
   public int getTotal () {
-    return 0;
+    return total;
   }
   
   public void add (int value) {
+	  total += value;
+	  history.concat(" + " + value);
   }
 
   public void subtract (int value) {
+	  total -= value;
+	  history.concat(" - " + value);
   }
 
   public String toString () {
-    return "";
+    return history;
   }
 
   public void clear() {
